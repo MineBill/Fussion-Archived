9661468c446b45da37904738ba4f121f825fe65f
MineBill <minebill@protonmail.com>
2023-01-15 02:32:21 +0200

    Cross-compiler trap function

--- Fussion/src/Fussion/Core.h
+++ Fussion/src/Fussion/Core.h
@@ -1,10 +1,28 @@
 #pragma once
 #include "Fussion/Log.h"
 
+#if defined(_MSC_VER)
+    #define BUILTIN_TRAP_FUNCTION __debugbreak
+#elif defined(__clang__) || defined(__GNUC__)
+    #define BUILTIN_TRAP_FUNCTION __builtin_trap
+#endif
+
 #ifdef FSN_USE_ASSERTIONS
-#define FSN_ASSERT(expr, ...) { if(!(expr)) { FSN_CLIENT_ERR("Assertion hit: {}", __VA_ARGS__); __debugbreak(); }}
-#define FSN_CORE_ASSERT(expr, ...) { if(!(expr)) { FSN_CORE_ERR("Assertion hit: {}", __VA_ARGS__); __debugbreak(); }}
+    #define FSN_ASSERT(expr, ...)                                 \
+        {                                                         \
+            if (!(expr)) {                                        \
+                FSN_CLIENT_ERR("Assertion hit: {}", __VA_ARGS__); \
+                BUILTIN_TRAP_FUNCTION();                          \
+            }                                                     \
+        }
+    #define FSN_CORE_ASSERT(expr, ...)                          \
+        {                                                       \
+            if (!(expr)) {                                      \
+                FSN_CORE_ERR("Assertion hit: {}", __VA_ARGS__); \
+                BUILTIN_TRAP_FUNCTION();                        \
+            }                                                   \
+        }
 #else
-#define FSN_ASSERT(expr, ...)
-#define FSN_CORE_ASSERT(expr, ...)
+    #define FSN_ASSERT(expr, ...)
+    #define FSN_CORE_ASSERT(expr, ...)
 #endif
