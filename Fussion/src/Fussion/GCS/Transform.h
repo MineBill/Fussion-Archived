f104f2de618e2d7c8ef34d6318e29dcb01d7a1d4
MineBill <minebill@protonmail.com>
2023-01-28 11:02:15 +0200

    Update Transform.h

--- Fussion/src/Fussion/GCS/Transform.h
+++ Fussion/src/Fussion/GCS/Transform.h
@@ -5,13 +5,28 @@ namespace Fussion
 {
     class Transform : public Fussion::Component
     {
-        glm::vec3 m_position{};
-        glm::vec3 m_scale{};
-        f32 m_rotation{};
+        glm::vec3 m_position{0, 0, 0};
+        glm::vec3 m_scale{1, 1, 1};
+        f32 m_rotation{0};
 
     public:
         FSN_COMPONENT(Transform)
 
         void on_editor_gui() override;
+
+        const glm::vec3 &position() const
+        {
+            return m_position;
+        }
+
+        const glm::vec3 &scale() const
+        {
+            return m_scale;
+        }
+
+        f32 rotation() const
+        {
+            return m_rotation;
+        }
     };
 } // namespace Fussion
