d2dd0d43b00e6c4f84e2cfe135d79b93a9ddd4fa
MineBill <minebill@protonmail.com>
2023-04-10 12:12:35 +0300

    Simple imgui helper to draw rect

--- Editor/ImGuiHelpers.h
+++ Editor/ImGuiHelpers.h
@@ -9,4 +9,6 @@ namespace ImGuiHelpers
     void DragVec3(const char *id, glm::vec3 *value, f32 speed = 1.0f, f32 min = 0.0f, f32 max = 0.0f,
                   const char *format = "%.2f");
     bool ButtonCenteredOnLine(const char *label, float alignment = 0.5f);
+    void RenderSimpleRect(ImDrawList *draw_list, glm::vec2 const &position, glm::vec2 const &size,
+                          u32 color = 0xff0000ff, f32 width = 1.0f);
 } // namespace ImGuiHelpers
